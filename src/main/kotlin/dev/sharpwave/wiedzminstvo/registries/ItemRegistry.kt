60f0cb6a936a1634dd7434c204b8a994471402d0
lai0n <karaba.peter12@gmail.com>
2021-08-16 20:13:23 +0200

    Removed BottomlessCarafeItem class and renamed AlchemyItem to AlchemyIngredientItem

--- src/main/kotlin/dev/sharpwave/wiedzminstvo/registries/ItemRegistry.kt
+++ src/main/kotlin/dev/sharpwave/wiedzminstvo/registries/ItemRegistry.kt
@@ -1,13 +1,9 @@
 package dev.sharpwave.wiedzminstvo.registries
 
 import dev.sharpwave.wiedzminstvo.WiedzminstvoMod
-import dev.sharpwave.wiedzminstvo.items.AlchemyItem
-import dev.sharpwave.wiedzminstvo.items.BottomlessCarafeItem
+import dev.sharpwave.wiedzminstvo.items.AlchemyIngredientItem
 import dev.sharpwave.wiedzminstvo.items.ModItemGroup
-import dev.sharpwave.wiedzminstvo.utils.DatagenHelpers.getRegistryPath
 import net.minecraft.block.Block
-import net.minecraft.block.TallFlowerBlock
-import net.minecraft.block.TallGrassBlock
 import net.minecraft.item.BlockItem
 import net.minecraft.item.Item
 import net.minecraft.item.ItemGroup
@@ -15,7 +11,6 @@ import net.minecraft.item.TallBlockItem
 import net.minecraftforge.registries.ForgeRegistries
 import thedarkcolour.kotlinforforge.eventbus.KotlinEventBus
 import thedarkcolour.kotlinforforge.forge.KDeferredRegister
-import thedarkcolour.kotlinforforge.forge.ObjectHolderDelegate
 
 object ItemRegistry : IForgeRegistry {
     private val ITEMS : KDeferredRegister<Item> = KDeferredRegister(ForgeRegistries.ITEMS, WiedzminstvoMod.MODID)
@@ -24,13 +19,13 @@ object ItemRegistry : IForgeRegistry {
         ITEMS.register(bus)
     }
 
-    val ALCHEMISTS_POWDER by ITEMS.registerObject("alchemists_powder") { AlchemyItem() }
-    val AETHER by ITEMS.registerObject("aether") { AlchemyItem() }
-    val ALCHEMY_PASTE by ITEMS.registerObject("alchemy_paste") { AlchemyItem() }
-    val ALCOHEST by ITEMS.registerObject("alcohest") { AlchemyItem() }
-    val DARK_ESSENCE by ITEMS.registerObject("dark_essence") { AlchemyItem() }
-    val DWARVEN_SPIRIT by ITEMS.registerObject("dwarven_spirit") { AlchemyItem() }
-    val FIFTH_ESSENCE by ITEMS.registerObject("fifth_essence") { AlchemyItem() }
+    val ALCHEMISTS_POWDER by ITEMS.registerObject("alchemists_powder") { AlchemyIngredientItem() }
+    val AETHER by ITEMS.registerObject("aether") { AlchemyIngredientItem() }
+    val ALCHEMY_PASTE by ITEMS.registerObject("alchemy_paste") { AlchemyIngredientItem() }
+    val ALCOHEST by ITEMS.registerObject("alcohest") { AlchemyIngredientItem() }
+    val DARK_ESSENCE by ITEMS.registerObject("dark_essence") { AlchemyIngredientItem() }
+    val DWARVEN_SPIRIT by ITEMS.registerObject("dwarven_spirit") { AlchemyIngredientItem() }
+    val FIFTH_ESSENCE by ITEMS.registerObject("fifth_essence") { AlchemyIngredientItem() }
     val ARENARIA by ITEMS.registerObject("arenaria") { registerBlock(BlockRegistry.ARENARIA, ModItemGroup.TAB_ALCHEMY) }
     // TODO: Maybe change beggartick and bison grass to flower or make implementation which also has offsetting as flower because it's weird how it is in middle of the block
     val BEGGARTICK by ITEMS.registerObject("beggartick_blossoms") { registerBlock(BlockRegistry.BEGGARTICK, ModItemGroup.TAB_ALCHEMY) }
